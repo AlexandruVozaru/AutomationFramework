364d4dd76d04e8eec0bf5f77ef7fc4533dc82801
AlexandruVozaru <62544690+AlexandruVozaru@users.noreply.github.com>
2022-09-20 19:51:11 +0300

    Update Hooks.java

--- src/test/java/Steps/Hooks.java
+++ src/test/java/Steps/Hooks.java
@@ -16,7 +16,7 @@ public class Hooks extends BaseUtil {
     @Before
     public void InitializeTheTest() {
         //  base.stepInfo="Chrome";
-        System.setProperty("webdriver.chrome.driver", "/Users/alexandru.vozaru/Desktop/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\Users\Alex\Desktop");
         base.Driver = new ChromeDriver();
 
     }
